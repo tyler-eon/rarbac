41e1047a95befacb43064de72ef682fb68e71185
Tyler Margison <kolorahl@gmail.com>
2014-07-10 23:21:35 -0700

    Fix associations for action and role models

--- app/models/rarbac/action.rb
+++ app/models/rarbac/action.rb
@@ -2,5 +2,6 @@ module Rarbac
   # Represents an action. Actions are linked to roles by way of the Permission
   # model.
   class Action < ActiveRecord::Base
+    has_many :permissions
   end
 end
--- app/models/rarbac/role.rb
+++ app/models/rarbac/role.rb
@@ -2,5 +2,6 @@ module Rarbac
   # Represents a role. Roles are linked to actions by way of the Permission
   # model.
   class Role < ActiveRecord::Base
+    has_many :permissions
   end
 end
