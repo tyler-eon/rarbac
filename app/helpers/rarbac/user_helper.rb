8a3bd00c44714ec3eb3ed079dc0e531798fa2d67
Tyler Margison <kolorahl@gmail.com>
2014-07-10 20:20:36 -0700

    Add user model helper functions
    Include `Rarbac::UserHelper` in the user model to gain access to RBAC functions.

---
+++ app/helpers/rarbac/user_helper.rb
@@ -0,0 +1,38 @@
+module Rarbac
+  # A collection of useful functions to be used by the user model.
+  #
+  #     include Rarbac::UserHelper
+  #
+  # That's it! Now you have access to some awesome RBAC helper functions.
+  module UserHelper
+    # Determines if the user has one or more of the given roles linked to it.
+    #
+    # @param [String] roles an argument list of one or more roles to check for.
+    # @return [true|false] true if the user is linked to at least one of the
+    #   given roles.
+    def has_role?(*roles)
+      throw Exception.new("Must supply at least one role.") if roles.empty?
+      roles.where(name: roles).count > 0
+    end
+
+    # Determines if the user has all of the given roles linked to it.
+    #
+    # @param [String] roles an argument list of one or more roles to check for.
+    # @return [true|false] true if the user is linked to all of the given roles.
+    def has_roles?(*roles)
+      throw Exception.new("Must supply at least one role.") if roles.empty?
+      roles.where(name: roles).count == roles.count
+    end
+
+    # Determines if the user has permission to a given action. If the action
+    # does not exist at all, it is assumed the action is publicly available.
+    #
+    # @param [String] action name of the action to check permission for.
+    # @return [true|false] true if the user has at least one role linked to it
+    #   with permission to the given action.
+    def has_permission?(action)
+      return true if Action.where(name: action).count == 0
+      actions.where(name: action).count > 0
+    end
+  end
+end
